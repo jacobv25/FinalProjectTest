29b8795f5f83b5a5614a000d8f6db05d8aabc9fc
Gabriel Bactol <gabrielbactol109@gmail.com>
2019-04-29 00:27:15 -0700

    Switched LearnMoreList code around
    So I accidentally wrote my code into the wrong class, so here is the fix.

--- app/src/main/java/edu/miracosta/finalprojecttest/LearnMoreActivity.java
+++ app/src/main/java/edu/miracosta/finalprojecttest/LearnMoreActivity.java
@@ -12,25 +12,9 @@ public class LearnMoreActivity extends AppCompatActivity {
 
     //TODO: Each button (Animals, Plants, Survival Tips) take the user to its specific List View.
 
-    //private DBHelper db;
-    private List<Plant> plantList;
-    private LearnMoreListAdapter learnMoreListAdapter;
-    private ListView learnMoreListView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_more);
-
-        //Adapter code not fully implemented
-        /*
-        db = new DBHelper(this);
-
-        gamesList = db.getAllGames();
-        learnMoreListAdapter = new LearnMoreListAdapter(this, R.layout.learn_more_list_item, plantList);
-
-        learnMoreListView = findViewById(R.id.learnMoreListView);
-        learnMoreListView.setAdapter(learnMoreListAdapter);
-        */
     }
 }
