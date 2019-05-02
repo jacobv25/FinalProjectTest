a0c0ad30aac880fc066230eae6a940b10bb04ec5
Jacob Valenzuela <32970534+jacobv25@users.noreply.github.com>
2019-05-01 19:41:06 -0700

    Added a single example JSON object and JSONLoader class.

---
+++ app/src/main/java/edu/miracosta/finalprojecttest/model/JSONLoader.java
@@ -0,0 +1,57 @@
+package edu.miracosta.finalprojecttest.model;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONLoader {
+
+    /**
+     * Loads JSON data from a file in the assets directory.
+     * @param context The activity from which the data is loaded.
+     * @throws IOException If there is an error reading from the JSON file.
+     */
+    public static List<Animal> loadJSONFromAsset(Context context) throws IOException {
+        List<Animal> allAnimals = new ArrayList<>();
+        String json;
+
+        InputStream is = context.getAssets().open("MusicEvents.json");
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        json = new String(buffer, "UTF-8");
+
+        try {
+            JSONObject jsonRootObject = new JSONObject(json);
+            JSONArray allAnimalsJSON = jsonRootObject.getJSONArray("MusicEvents");
+            int numberOfEvents = allAnimalsJSON.length();
+
+            for (int i = 0; i < numberOfEvents; i++) {
+                JSONObject animalJSON = allAnimalsJSON.getJSONObject(i);
+
+                Animal animal = new Animal();
+
+                //Extract the Artist from the JSON object
+                animal.setAnimalName(animalJSON.getString("Name"));
+                animal.setAnimalDescription(animalJSON.getString("Description"));
+                animal.setAnimalImage(animalJSON.getString("ImageName"));
+                allAnimals.add(animal);
+            }
+        }
+        catch (JSONException e)
+        {
+            Log.e("Final project cs134", e.getMessage());
+        }
+
+        return allAnimals;
+    }
+}
